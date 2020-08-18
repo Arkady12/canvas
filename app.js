59a07ac5c1ce1c80aee9459a9badfcf260e4ba7c
Arkady Maslak <bankrut9@gmail.com>
2020-08-18 23:30:42 +0300

    Сделал сброс состояния при гейм овере

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext('2d');
 
-var snake = [
-    [3, 2],
-    [2, 2],
-]
-var food = [9,9]
-var direction = 'right'
-var lastDirection = direction
+var snake
+var food
+var direction
+var lastDirection
 
 var blockWidth = canvas.width/10
 var blockHeight = canvas.height/10
+function resetState(){
+    snake = [
+        [3, 2],
+        [2, 2],
+    ]
+    food = [9,9]
+    direction = 'right'
+    lastDirection = direction
+}
 
 function blockCoordsToPixels(blockCoords){
     return [
@@ -91,12 +97,14 @@ function checkGameOver(){
     var isHeadInBody = snake.some(function(blockCoords,index){
         if (blockCoords==snake[0]) {
             return false
+            
         }
 
         return checkSamePosition(snake[0], blockCoords)
     })
     if(snake[0][0] == 0 || snake[0][0]==11 || snake[0][1]==0 || snake[0][1]==11 || isHeadInBody){
         alert('game over')
+        resetState()
     }
 }
 function draw(){
@@ -110,60 +118,48 @@ function step (){
     draw()
     lastDirection = direction
 }
-
+resetState()
 draw()
+setInterval(step,500)
+
+function tryToRotate (dir) {
+    var graph = {
+        left: ['up', 'down'],
+        right: ['up', 'down'],
+        up: ['left', 'right'],
+        down: ['left', 'right'],
+    }
+
+    var availableDirections = graph[lastDirection]
+    if(availableDirections.includes(dir)){
+        direction = dir
+    }
+}
 
 document.addEventListener('keydown', function(event){
-    if (event.code=="Space"){
-       step()
-    }
-    
-    
-    
-    if (lastDirection=='down' || lastDirection=='up'){
-        if (event.code=="ArrowLeft"){
-            direction='left'
-        }
-    
-        if (event.code=="ArrowRight"){
-            direction='right'
-        }
-    }
-    if (lastDirection=='left' || lastDirection=='right'){
-        if (event.code=="ArrowDown"){
-            direction='down'
-        }
-    
-        if (event.code=="ArrowUp"){
-            direction='up'
-        }
+    var directionMap = {
+        ArrowLeft: "left",
+        ArrowRight: "right",
+        ArrowDown: "down",
+        ArrowUp: "up",
     }
 
+    tryToRotate(directionMap[event.code])
 })
 var topButton = document.getElementsByClassName("top")[0]
 var leftButton = document.getElementsByClassName("left")[0]
 var rightButton = document.getElementsByClassName("right")[0]
 var downButton = document.getElementsByClassName("down")[0]
 
-topButton.addEventListener('click',function(){
-    if (direction=='left' || direction=='right'){
-      direction='up'
-  }
+topButton.addEventListener('click',function() {
+    tryToRotate('up')
 })
 leftButton.addEventListener('click',function(){
-    if (direction=='up' || direction=='down'){
-      direction='left'
-  }
+    tryToRotate('left')
  })
  rightButton.addEventListener('click',function(){
-    if (direction=='up' || direction=='down'){
-        direction='right'
-    }
+    tryToRotate('right')
  })
  downButton.addEventListener('click',function(){
-    if (direction=='left' || direction=='right'){
-        direction='down'
-    }
+    tryToRotate('down')
  })
- var moveButton = document.getElementsByClassName("move")[0]
- moveButton.addEventListener('click', step)

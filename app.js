be83207665308250799f80b20f829cceee5551e4
Arkady Maslak <bankrut9@gmail.com>
2020-10-06 21:35:43 +0300

    сделал текстурку змейки

--- app.js
+++ app.js
@@ -36,6 +36,20 @@ function blockCoordsToPixels(blockCoords){
 function clearCanvas(){
     ctx.clearRect(0,0,canvas.width,canvas.height)
 } 
+
+function whereMoving (a, b) {
+    if (a[0] < b[0]){
+        return "right"
+    }
+    if (a[0] > b[0]) {
+        return "left"
+    }
+    if (a[1] > b[1]) {
+        return "up"
+    }
+    return "down"
+}
+
 function drawSnake (){
     snake.forEach(function(blockCoords,index){
         var coordsInPixels = blockCoordsToPixels(blockCoords)
@@ -54,11 +68,105 @@ function drawSnake (){
             if(direction=='down'){
                 ctx.drawImage(pic, 256, 64, 64, 64, coordsInPixels[0],coordsInPixels[1], blockSize,blockSize);
             }
+        } else if(index==snake.length-1) {
+            var pred = snake[index-1]
+            var blockDirecttion = whereMoving(blockCoords, pred)
+            if (blockDirecttion == "left"){
+                ctx.drawImage(pic, 192, 192, 64, 64, coordsInPixels[0],coordsInPixels[1], blockSize,blockSize); 
+            }
+            if (blockDirecttion == "right"){
+                ctx.drawImage(pic, 256, 128, 64, 64, coordsInPixels[0],coordsInPixels[1], blockSize,blockSize);
+            }
+            if (blockDirecttion == "up"){
+                
+                ctx.drawImage(pic, 192, 128, 64, 64, coordsInPixels[0],coordsInPixels[1], blockSize,blockSize);
+            }
+            if (blockDirecttion == "down"){
+                ctx.drawImage(pic, 256, 192, 64, 64, coordsInPixels[0],coordsInPixels[1], blockSize,blockSize);
+            }
+
         } else {
-            ctx.fillStyle ='#000000'
-            ctx.fillRect(coordsInPixels[0],coordsInPixels[1],blockSize,blockSize )
+            var pred = snake[index-1]
+            var next = snake[index+1]
+            if (
+                (
+                    whereMoving(pred, blockCoords) == 'right' &&
+                    whereMoving(next,blockCoords) == 'down'
+                ) ||
+                (
+                    whereMoving(pred, blockCoords) == 'down' &&
+                    whereMoving(next,blockCoords) == 'right'
+                )
+            ) 
+            {
+                ctx.drawImage(pic, 128, 128, 64, 64, coordsInPixels[0],coordsInPixels[1], blockSize,blockSize);
+            }
+            if (
+                (
+                    whereMoving(pred, blockCoords) == 'up' &&
+                    whereMoving(next,blockCoords) == 'right'
+                ) ||
+                (
+                    whereMoving(pred, blockCoords) == 'right' &&
+                    whereMoving(next,blockCoords) == 'up'
+                )
+            ) 
+            {
+                ctx.drawImage(pic, 128, 0, 64, 64, coordsInPixels[0],coordsInPixels[1], blockSize,blockSize);
+            }
+            if (
+                (
+                    whereMoving(pred, blockCoords) == 'left' &&
+                    whereMoving(next,blockCoords) == 'up'
+                ) ||
+                (
+                    whereMoving(pred, blockCoords) == 'up' &&
+                    whereMoving(next,blockCoords) == 'left'
+                )
+            ) 
+            {
+                ctx.drawImage(pic, 0, 0, 64, 64, coordsInPixels[0],coordsInPixels[1], blockSize,blockSize);
+            }
+            if (
+                (
+                    whereMoving(pred, blockCoords) == 'down' &&
+                    whereMoving(next,blockCoords) == 'left'
+                ) ||
+                (
+                    whereMoving(pred, blockCoords) == 'left' &&
+                    whereMoving(next,blockCoords) == 'down'
+                )
+            ) 
+            {
+                ctx.drawImage(pic, 0, 64, 64, 64, coordsInPixels[0],coordsInPixels[1], blockSize,blockSize);
+            }
+            if (
+                (
+                    whereMoving(pred, blockCoords) == 'right' &&
+                    whereMoving(next,blockCoords) == 'left'
+                ) ||
+                (
+                    whereMoving(pred, blockCoords) == 'left' &&
+                    whereMoving(next,blockCoords) == 'right'
+                )
+            ) 
+            {
+                ctx.drawImage(pic, 64, 0, 64, 64, coordsInPixels[0],coordsInPixels[1], blockSize,blockSize);
+            }
+            if (
+                (
+                    whereMoving(pred, blockCoords) == 'down' &&
+                    whereMoving(next,blockCoords) == 'up'
+                ) ||
+                (
+                    whereMoving(pred, blockCoords) == 'up' &&
+                    whereMoving(next,blockCoords) == 'down'
+                )
+            ) 
+            {
+                ctx.drawImage(pic, 128, 64, 64, 64, coordsInPixels[0],coordsInPixels[1], blockSize,blockSize);
+            }
         }
-        
     })  
 }
 function drawFood (){
